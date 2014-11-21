1aa37dd356e466c1bbf993517b2448227a60b9f4
Raihan  Samprit <raihansa@ug169.eecg>
2014-11-20 20:42:56 -0500

    Included simple file i/o functionality to codegen

--- codegen.c
+++ codegen.c
@@ -6,4 +6,16 @@ Samprit Raihan, 998138830
 
 void generateAssembly(node * ast){
 	printf("Generating assembly.....\n");
+	// Open the assembly file
+	FILE *filePointer;	
+	filePointer = fopen("./frag.txt", "w");
+	
+	/*
+		From this point on, keep adding new instructions
+		to the assembly file on a new line using 
+		fprint(filePointer, <newline terminated string>)
+	*/
+	
+	// Close the assembly file
+	fclose(filePointer);
 }
